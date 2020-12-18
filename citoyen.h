0b59d5119c8939cc84a21e55097326a2d2156413
Ahmed <75306161+azerty0004@users.noreply.github.com>
2020-12-18 01:32:25 +0100

    Add files via upload

--- citoyen.h
+++ citoyen.h
@@ -1,11 +1,37 @@
 #ifndef CITOYEN_H
 #define CITOYEN_H
+#include <QString>
+#include <QDate>
+#include <QSqlQuery>
+#include <QSqlQueryModel>
+
 
 
 class citoyen
-{
+{private:
+    QString nom,prenom,CIN;
+    QDate date_naissance;
+
 public:
     citoyen();
+    citoyen(QString,QString,QString,QDate);
+
+    QString Getnom() {return nom;}
+    void Setnom(QString a){nom=a;}
+
+    QString getprenom() {return prenom;}
+    void Setprenomnom(QString a){nom=a;}
+
+    QString GetCIN() { return CIN;}
+    void SetCIN(QString a){CIN=a;}
+
+    QDate Getdate_naissance(){return  date_naissance;}
+    void Setdate_naissance(QDate a) {date_naissance=a;}
+
+
+
+
+
 };
 
 #endif // CITOYEN_H
